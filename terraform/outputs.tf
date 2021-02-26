76d490c53ca4d24eee594200fec002684ba8246f
q1hlw6 <q1hlw6@protonmail.com>
2021-02-26 16:03:17 +0100

    Add ECR repository for sample Docker images

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "kubernetes_endpoint" {
 output "kubernetes_certificate_authority_data" {
   value = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
 }
+
+output "docker_repository_url" {
+  value = aws_ecr_repository.sample.repository_url
+}
