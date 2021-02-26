8cd5799014bb5bd4315af46577e939de79f6308c
q1hlw6 <q1hlw6@protonmail.com>
2021-02-26 15:01:02 +0100

    Add basic EKS cluster and Kubernetes provider

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.29.1"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.0.2"
+    }
   }
 }
 
@@ -21,6 +25,9 @@ locals {
   vpc_cidr            = "172.16.0.0/16"
   vpc_private_subnets = ["172.16.0.0/24", "172.16.1.0/24", "172.16.2.0/24"]
   vpc_public_subnets  = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+
+  cluster_name    = var.project_name
+  cluster_version = "1.19"
 }
 
 module "vpc" {
@@ -33,4 +40,31 @@ module "vpc" {
   azs             = data.aws_availability_zones.all.names
   private_subnets = local.vpc_private_subnets
   public_subnets  = local.vpc_public_subnets
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "14.0.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = local.cluster_version
+
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
+
+  write_kubeconfig = false
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
